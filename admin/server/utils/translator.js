960bd34876c28a079ce63d55873461b521311c1e
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 12:36:08 +0700

    Update translator.js

--- admin/server/utils/translator.js
+++ admin/server/utils/translator.js
@@ -1,1 +1,15 @@
 
+// ใช้ Google Translate API (mock example)
+const translate = require('@vitalets/google-translate-api');
+
+async function translateText(text, targetLang='th') {
+    try {
+        const res = await translate(text, {to: targetLang});
+        return res.text;
+    } catch (err) {
+        console.error('Translation error:', err);
+        return text;
+    }
+}
+
+module.exports = { translateText };
