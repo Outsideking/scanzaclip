c5700b534fb44f86886ab60608156fb6a9165e82
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 12:10:55 +0700

    Update Role.js

--- permissions/models/Role.js
+++ permissions/models/Role.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
 
+const RoleSchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    description: { type: String },
+    permissions: { type: [String], default: [] } // list ของสิทธิ์เช่น ["CREATE_SCLIP", "DELETE_SCLIP"]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Role', RoleSchema);
