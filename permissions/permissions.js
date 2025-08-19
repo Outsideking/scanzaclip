4cdf24d511a937c80fead502dd597992478b02b9
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 12:12:24 +0700

    Update permissions.js

--- permissions/permissions.js
+++ permissions/permissions.js
@@ -1,1 +1,13 @@
+function checkPermission(permission) {
+    return async (req, res, next) => {
+        const user = req.user; // สมมติว่า req.user ถูก set จาก authentication
+        if (!user) return res.status(401).send('Unauthorized');
 
+        const hasPermission = user.roles.some(role => role.permissions.includes(permission));
+        if (!hasPermission) return res.status(403).send('Forbidden');
+
+        next();
+    };
+}
+
+module.exports = { checkPermission };
