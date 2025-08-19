cce1746d4a233c488aaba9ff266d27aa62b43068
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 12:18:58 +0700

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -1,1 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const sclipRoutes = require('./routes/sclip');
 
+app.use(express.json());
+
+// connect MongoDB
+mongoose.connect('mongodb://localhost:27017/scanzaclip', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// admin routes
+app.use('/admin/sclips', sclipRoutes);
+
+app.listen(4000, () => {
+    console.log('Admin API running on http://localhost:4000');
+});
