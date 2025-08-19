ecfc2d4e2b24004aaeccd0f8d525b0b7e3de0895
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 20:20:42 +0700

    Update db.js

--- packages/     shared/db.js
+++ packages/     shared/db.js
@@ -1,1 +1,16 @@
+// packages/shared/db.js
+import { MongoClient } from "mongodb";
 
+const uri = process.env.MONGO_URI || "mongodb://localhost:27017/scanzaclip";
+const client = new MongoClient(uri);
+
+export async function saveToDatabase(collectionName, data) {
+  try {
+    await client.connect();
+    const db = client.db("scanzaclip");
+    const collection = db.collection(collectionName);
+    await collection.insertOne(data);
+  } catch (err) {
+    console.error("DB error:", err);
+  }
+}
