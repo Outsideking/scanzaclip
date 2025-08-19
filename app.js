2002d100b0a00df3dfca668d1ca43a1eac12b4b5
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 12:13:49 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const roleRoutes = require('./permissions/routes/roles');
 
+app.use(express.json());
+
+// connect MongoDB
+mongoose.connect('mongodb://localhost:27017/scanzaclip', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use('/api/roles', roleRoutes);
+
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
