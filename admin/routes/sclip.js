924f8949cd75b8e0981e4d739c9b30168009fb09
Thanva Phupingbut <rufiodinoto244@gmail.com>
2025-08-19 12:18:21 +0700

    Update sclip.js

--- admin/routes/sclip.js
+++ admin/routes/sclip.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const sclipController = require('../controllers/sclipController');
+const { checkPermission } = require('../../permissions/permissions');
 
+// ใช้ middleware ตรวจสอบสิทธิ์
+router.post('/', checkPermission('CREATE_SCLIP'), sclipController.createSclip);
+router.get('/', checkPermission('READ_SCLIP'), sclipController.getSclips);
+router.put('/:id', checkPermission('UPDATE_SCLIP'), sclipController.updateSclip);
+router.delete('/:id', checkPermission('DELETE_SCLIP'), sclipController.deleteSclip);
+
+module.exports = router;
